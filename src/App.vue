ce3bfe71579f819ed44a04f0570c100a0eda0135
Evelyne Akoth <evelyneoakoth@gmail.com>
2018-06-20 16:02:46 +0300

    added a file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
-    <h3>{{ title }}</h3>
+    <h3>{{ title }} </h3>
+    <form>
+    <textarea placeholder="Add new task" v-model="newTask"></textarea>
+    <p>{{newTask}}</p>
+    <button type="button" v-on:click="addTask">Add Task {{tasks.length}}</button>
+    </form>
+    <ul>
+    <li v-for="task in tasks">{{task}}</li>
+    </ul>
+    
+
+
   </div>
 </template>
 
@@ -8,8 +19,24 @@
 export default {
   data: function() {
     return {
-      title: 'My App'
+      title: 'My Todo list',
+      newTask:'This is me',
+      
+      
+      tasks:[
+         
+      ]
+      
+      
+       
     }
+  },
+  methods:{
+  	addTask(){
+  		this.tasks.unshift(this.newTask);
+  		this.newTask = '';
+
+  	}
   }
 }
 </script>
